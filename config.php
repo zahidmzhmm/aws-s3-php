345fed3f38027fba6aaa1307dc155f2ae9035686
Zahid Mzhmm <64451438+zahidmzhmm@users.noreply.github.com>
2021-08-22 21:26:36 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('AWS_Bucket_Name', 'app.lectureverb');
-define('AWS_IAM_KEY', 'AKIA5CP4UJWPCYCA6EUW');
-define('AWS_IAM_SECRET', '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');
+define('AWS_IAM_KEY', '***');
+define('AWS_IAM_SECRET', '***');
 define('AWS_REGION', 'eu-west-2');
 require 'aws/autoload.php';
 require 'Upload.php';
