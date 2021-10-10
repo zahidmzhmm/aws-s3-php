a2692b7da7b3945e8a5cf53f94d5aa281f57d037
Zahid Mzhmm <64451438+zahidmzhmm@users.noreply.github.com>
2021-10-10 19:10:31 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('AWS_Bucket_Name', 'app.lectureverb');
+define('AWS_Bucket_Name', 'app.fazgroupltd');
 define('AWS_IAM_KEY', '***');
 define('AWS_IAM_SECRET', '***');
 define('AWS_REGION', 'eu-west-2');
